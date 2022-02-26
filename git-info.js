dca2c49caf925cc2954e3761b6ee2184182947e4
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-02-26 12:26:41 -0600

    made silly mistake

--- git-info.js
+++ git-info.js
@@ -1,9 +1,9 @@
 const GITHUB_DATA = {
-    commit_sha = "COMMITSHA",
-    commit_author = "COMMITAUTHOR",
-    action_event_name = "EVENTNAME",
+    commit_sha:"COMMITSHA",
+    commit_author:"COMMITAUTHOR",
+    action_event_name:"EVENTNAME",
     
-    job_id = "JOBID",
-    github_repository = "GITHUBREPOSITORY",
-    github_actor = "GITHUBACTOR"
+    job_id:"JOBID",
+    github_repository:"GITHUBREPOSITORY",
+    github_actor:"GITHUBACTOR"
 }
