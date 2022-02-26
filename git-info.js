7d4e3ead04ba5c5f6af2fe6eeeac5b193844da78
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-02-26 12:20:07 -0600

    removed git-info data for a test

--- git-info.js
+++
@@ -1,9 +0,0 @@
-const GITHUB_DATA = {
-    commit_sha = "COMMITSHA",
-    commit_author = "COMMITAUTHOR",
-    action_event_name = "EVENTNAME",
-    
-    job_id = "JOBID",
-    github_repository = "GITHUBREPOSITORY",
-    github_actor = "GITHUBACTOR"
-}
