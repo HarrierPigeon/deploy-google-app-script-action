28100c26e53ab35e0dd85e807832d53129c7a0d8
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-02-26 12:22:44 -0600

    .

---
+++ git-info.js
@@ -0,0 +1,9 @@
+const GITHUB_DATA = {
+    commit_sha = "COMMITSHA",
+    commit_author = "COMMITAUTHOR",
+    action_event_name = "EVENTNAME",
+    
+    job_id = "JOBID",
+    github_repository = "GITHUBREPOSITORY",
+    github_actor = "GITHUBACTOR"
+}
